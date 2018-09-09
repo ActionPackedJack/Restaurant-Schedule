92d38dc8fb1692dc079668a68c275a6e97bf0eb5
ActionPackedJack <>
2018-09-09 01:50:01 -0500

    Took function out of ngOnInit() and enabled it to read the database.

--- public/src/app/schedule/schedule.component.ts
+++ public/src/app/schedule/schedule.component.ts
@@ -9,14 +9,13 @@ import {HttpService} from '../http.service';
   styleUrls: ['./schedule.component.css']
 })
 export class ScheduleComponent implements OnInit {
-
-  constructor(private _dataService: HttpService, private _route: ActivatedRoute, private router: Router) { }
-
-  ngOnInit() {
-    var employees = [];
-    this.getEmployees();
-    console.log("HERE ARE EMPLOYEES:", employees)
-    var schedule = {
+  employees: any[];
+  schedule: any;
+  problems: any[];
+  doubles: any[];
+  constructor(private _dataService: HttpService, private _route: ActivatedRoute, private router: Router) {
+    this.employees= [];
+    this.schedule = {
       mondayAM: {},
       mondayPM: {},
       tuesdayAM: {},
@@ -32,39 +31,29 @@ export class ScheduleComponent implements OnInit {
       sundayAM: {},
       sundayPM: {},
     };
-    var problems= [];
+    this.problems= [];
     
-    var doubles = [];
+    this.doubles = [];
+   }
 
-    //The below code automatically assigns extra properties to each employee to help the algorithm make decisions
+  ngOnInit() {
+    //var employees = [];
+    this.getEmployees();
+    //employees = this.getEmployees();
+    
 
-for (var i = 0; i < employees.length; i++) {
-	var server = employees[i];
-	//We keep track of the number of shifts that have been scheduled this week so that the limits will not be exceeded.
-	server.shiftsScheduled = 0;
-	server.bartenderScheduled = 0;
-	server.shiftLeaderScheduled = 0;
-	server.availability = 0;
-	for (var key in server.shifts) {
-		if (server.requests.indexOf(key.toString()) > -1) {
-			server.shifts[key] = false;
-		}
-		if (server.shifts[key] === true) {
-			server.availability++;
-		}
-	}
-	server.priority = server.shiftsPerWeek + 14 - server.availability;
-	//Priority decides who is scheduled when multiple employees can work the same shift. An employee will receive higher priority if there are less other shifts available for them, to guarantee that someone who can only work one particular shift will always get it. An employee who can work more shifts per week will receive higher priority; their priority will be higher than that of an employee with open availability who can only work once a week, but lower than that of a server who can only work one specific shift.
-}
+  }
+
+
 
 //The below function checks whether the shift currently being scheduled is a morning. If it is, it returns the night shift of the same day.
-function isMorning(shift) {
+isMorning(shift) {
 	var str = shift.toString();
 	if (str[str.length - 2] === 'P') {
 		return "false";
 	}
 	var night = str.slice(0, str.length - 2) + 'PM';
-	for (var key in employees[0].shifts) {
+	for (var key in this.employees[0].shifts) {
 		if (key.toString() === night) {
 			return key;
 		}
@@ -72,35 +61,35 @@ function isMorning(shift) {
 }
 
 //The below function sorts the employees in order of highest to lowest based on priority, so that the scheduling function can evaluate them in the proper order.
-function prioritySort(arr) {
-	for (var i = 1; i < employees.length; i++) {
-		if (employees[i].priority > employees[i - 1].priority) {
-			var temp = employees[i - 1];
-			employees[i - 1] = employees[i];
-			employees[i] = temp;
+prioritySort(arr) {
+	for (var i = 1; i < this.employees.length; i++) {
+		if (this.employees[i].priority > this.employees[i - 1].priority) {
+			var temp = this.employees[i - 1];
+			this.employees[i - 1] = this.employees[i];
+			this.employees[i] = temp;
 			i = 1;
 		}
 	}
 	return arr;
 }
 //This algorithm will not schedule an employ to a double shift, because anyone scheduled in the morning will be marked as unavailable for the evening. In the event that a PM shift cannot be filled, the below code will check and see if there is anyone eligible to work it who is already scheduled that morning, and return a list of employees who could be scheduled to a double if necessary.
-function doublesCheck(shift){
+doublesCheck(shift){
   var result=[]
-  for (let i=0; i<doubles.length; i++){
-    if(doubles[i].indexOf(shift)===0){
-      result.push(doubles[i].slice(shift.length+1, doubles[i].length));
+  for (let i=0; i<this.doubles.length; i++){
+    if(this.doubles[i].indexOf(shift)===0){
+      result.push(this.doubles[i].slice(shift.length+1, this.doubles[i].length));
     }
   }
   return result;
 }
 //The below function schedules the bartender for a single shift. This will be run on every day first, so that qualified bartenders will not be scheduled to serving shifts until all bartender shifts are filled.
-function scheduleBartender(shift) {
-  for(var key in schedule){
+scheduleBartender(shift) {
+  for(var key in this.schedule){
     if(key.toString()===shift.toString()){
       var shift:any=key;
     }
   }
-	var sortedEmployees = prioritySort(employees);
+	var sortedEmployees = this.prioritySort(this.employees);
 	for (var i = 0; i < sortedEmployees.length; i++) {
 		var server = sortedEmployees[i];
 		if (
@@ -109,31 +98,31 @@ function scheduleBartender(shift) {
 			server.shifts[shift] === true
 		) {
       console.log("Scheduling " + server.name + " on " + shift + " as bartender.")
-      schedule[shift].bartender = server.name;
+      this.schedule[shift].bartender = server.name;
 			shift.bartender = server.name;
 			server.bartenderScheduled++;
 			server.shiftsScheduled++;
       server.shifts[shift] = false;
 			//If this is a morning shift, the below code checks that server's availability for the corresponding night shift. If they are available, their availability will be set to false but a note will be made in their file that this has happened in case the manager needs to schedule someone to a double shift.
-			if (isMorning(shift !="false")) {
-        if(server.shifts[isMorning(shift)]===true){
-				  server.shifts[isMorning(shift)] = false;
-				  doubles.push(isMorning(shift) + " " + server.name);
+			if (this.isMorning(shift !="false")) {
+        if(server.shifts[this.isMorning(shift)]===true){
+				  server.shifts[this.isMorning(shift)] = false;
+				  this.doubles.push(this.isMorning(shift) + " " + server.name);
         }
 			}
       return;
 		}
 	}
-  problems.push("Could not find eligible bartender on " + shift + ".")
+  this.problems.push("Could not find eligible bartender on " + shift + ".")
 }
 //The below code is nearly identical to the bartender scheduling logic, but handles shift leaders.
-function scheduleShiftLeader(shift) {
-  for(var key in schedule){
+scheduleShiftLeader(shift) {
+  for(var key in this.schedule){
     if(key.toString()===shift.toString()){
       var shift:any=key;
     }
   }
-	var sortedEmployees = prioritySort(employees);
+	var sortedEmployees = this.prioritySort(this.employees);
 	for (var i = 0; i < sortedEmployees.length; i++) {
 		var server = sortedEmployees[i];
 		if (
@@ -142,25 +131,25 @@ function scheduleShiftLeader(shift) {
 			server.shifts[shift] === true
 		) {
       console.log("Scheduling " + server.name + " on " + shift + " as shift leader.")
-			schedule[shift].section1 = server.name;
+			this.schedule[shift].section1 = server.name;
 			server.shiftLeaderScheduled++;
 			server.shiftsScheduled++;
       server.shifts[shift] = false;
-			if (isMorning(shift !="false")) {
-        if(server.shifts[isMorning(shift)]===true){
-				  server.shifts[isMorning(shift)] = false;
-				  server.doubles.push(isMorning(shift));
+			if (this.isMorning(shift !="false")) {
+        if(server.shifts[this.isMorning(shift)]===true){
+				  server.shifts[this.isMorning(shift)] = false;
+				  server.doubles.push(this.isMorning(shift));
         }
 			}
       return;
 		}
 	}
-  problems.push("Could not find eligible shift leader on " + shift + ".");
+  this.problems.push("Could not find eligible shift leader on " + shift + ".");
 }
 
 //The below code fills out the shift with non-shift-leader, non-bartender servers. totalServers represents the total number of people working the shift, including shift leader and bartender.
-function scheduleRemainder(shift, totalServers=5){
-  var sortedEmployees = prioritySort(employees);
+scheduleRemainder(shift, totalServers=5){
+  var sortedEmployees = this.prioritySort(this.employees);
   for(var i =2; i< totalServers; i++){
     var section = "section" + i.toString();
     for (var q = 0; q < sortedEmployees.length; q++) {
@@ -170,13 +159,13 @@ function scheduleRemainder(shift, totalServers=5){
 			  server.shifts[shift] === true
 		  ) {
         console.log("Scheduling " + server.name + " on " + shift + " as " + section + ".")
-			  schedule[shift][section] = server.name;
+			  this.schedule[shift][section] = server.name;
 			  server.shiftsScheduled++;
         server.shifts[shift] = false;
-			  if (isMorning(shift !="false")) {
-          if(server.shifts[isMorning(shift)]===true){
-				    server.shifts[isMorning(shift)] = false;
-				    server.doubles.push(isMorning(shift));
+			  if (this.isMorning(shift !="false")) {
+          if(server.shifts[this.isMorning(shift)]===true){
+				    server.shifts[this.isMorning(shift)] = false;
+				    server.doubles.push(this.isMorning(shift));
           }
 			  }
         break;
@@ -184,10 +173,10 @@ function scheduleRemainder(shift, totalServers=5){
         continue;
     }
     let problem = ("Could not find employee to work " + section + " on " + shift + ".");
-    if(doublesCheck(shift).length>0){
-      problem+= (" Potential doubles: " + doublesCheck(shift));
+    if(this.doublesCheck(shift).length>0){
+      problem+= (" Potential doubles: " + this.doublesCheck(shift));
     }
-    problems.push(problem);
+    this.problems.push(problem);
   }
 }
 // scheduleBartender("mondayAM");
@@ -199,7 +188,7 @@ function scheduleRemainder(shift, totalServers=5){
 // console.log(schedule);
 // console.log(problems)
 
-function makeSchedule(
+makeSchedule(
   fridayAMServers=7, 
   fridayPMServers=7, 
   saturdayAMServers=7, 
@@ -215,67 +204,86 @@ function makeSchedule(
   thursdayAMServers=5,
   thursdayPMServers=5)
   {
-    scheduleBartender("fridayAM");
-    scheduleBartender("fridayPM");
-    scheduleBartender("saturdayAM");
-    scheduleBartender("saturdayPM");
-    scheduleBartender("sundayAM");
-    scheduleBartender("sundayPM");
-    scheduleBartender("thursdayAM");
-    scheduleBartender("thursdayPM");
-    scheduleBartender("wednesdayAM");
-    scheduleBartender("wednesdayPM");
-    scheduleBartender("tuesdayAM");
-    scheduleBartender("tuesdayPM");
-    scheduleBartender("mondayAM");
-    scheduleBartender("mondayPM");
-    scheduleShiftLeader("fridayAM");
-    scheduleShiftLeader("fridayPM");
-    scheduleShiftLeader("saturdayAM");
-    scheduleShiftLeader("saturdayPM");
-    scheduleShiftLeader("sundayAM");
-    scheduleShiftLeader("sundayPM");
-    scheduleShiftLeader("thursdayAM");
-    scheduleShiftLeader("thursdayPM");
-    scheduleShiftLeader("wednesdayAM");
-    scheduleShiftLeader("wednesdayPM");
-    scheduleShiftLeader("tuesdayAM");
-    scheduleShiftLeader("tuesdayPM");
-    scheduleShiftLeader("mondayAM");
-    scheduleShiftLeader("mondayPM");
-    scheduleRemainder("fridayAM", fridayAMServers);
-    scheduleRemainder("fridayPM", fridayPMServers);
-    scheduleRemainder("saturdayAM", saturdayAMServers);
-    scheduleRemainder("saturdayPM", saturdayPMServers);
-    scheduleRemainder("sundayAM", sundayAMServers);
-    scheduleRemainder("sundayPM", sundayPMServers);
-    scheduleRemainder("thursdayAM", thursdayAMServers);
-    scheduleRemainder("thursdayPM", thursdayPMServers);
-    scheduleRemainder("wednesdayAM", wednesdayAMServers);
-    scheduleRemainder("wednesdayPM", wednesdayPMServers);
-    scheduleRemainder("tuesdayAM", tuesdayAMServers);
-    scheduleRemainder("tuesdayPM", tuesdayPMServers);
-    scheduleRemainder("mondayAM", mondayAMServers);
-    scheduleRemainder("mondayPM", mondayPMServers);
-    console.log(schedule);
-    for(let q=0; q<employees.length;q++){
-      if(employees[q].shiftsPerWeek > employees[q].shiftsScheduled){
-        problems.push(employees[q].name + " did not get the desired amount of shifts (" + employees[q].shiftsPerWeek + " expected, " + employees[q].shiftsScheduled + " received).")
+    console.log(this.employees);
+    this.scheduleBartender("fridayAM");
+    this.scheduleBartender("fridayPM");
+    this.scheduleBartender("saturdayAM");
+    this.scheduleBartender("saturdayPM");
+    this.scheduleBartender("sundayAM");
+    this.scheduleBartender("sundayPM");
+    this.scheduleBartender("thursdayAM");
+    this.scheduleBartender("thursdayPM");
+    this.scheduleBartender("wednesdayAM");
+    this.scheduleBartender("wednesdayPM");
+    this.scheduleBartender("tuesdayAM");
+    this.scheduleBartender("tuesdayPM");
+    this.scheduleBartender("mondayAM");
+    this.scheduleBartender("mondayPM");
+    this.scheduleShiftLeader("fridayAM");
+    this.scheduleShiftLeader("fridayPM");
+    this.scheduleShiftLeader("saturdayAM");
+    this.scheduleShiftLeader("saturdayPM");
+    this.scheduleShiftLeader("sundayAM");
+    this.scheduleShiftLeader("sundayPM");
+    this.scheduleShiftLeader("thursdayAM");
+    this.scheduleShiftLeader("thursdayPM");
+    this.scheduleShiftLeader("wednesdayAM");
+    this.scheduleShiftLeader("wednesdayPM");
+    this.scheduleShiftLeader("tuesdayAM");
+    this.scheduleShiftLeader("tuesdayPM");
+    this.scheduleShiftLeader("mondayAM");
+    this.scheduleShiftLeader("mondayPM");
+    this.scheduleRemainder("fridayAM", fridayAMServers);
+    this.scheduleRemainder("fridayPM", fridayPMServers);
+    this.scheduleRemainder("saturdayAM", saturdayAMServers);
+    this.scheduleRemainder("saturdayPM", saturdayPMServers);
+    this.scheduleRemainder("sundayAM", sundayAMServers);
+    this.scheduleRemainder("sundayPM", sundayPMServers);
+    this.scheduleRemainder("thursdayAM", thursdayAMServers);
+    this.scheduleRemainder("thursdayPM", thursdayPMServers);
+    this.scheduleRemainder("wednesdayAM", wednesdayAMServers);
+    this.scheduleRemainder("wednesdayPM", wednesdayPMServers);
+    this.scheduleRemainder("tuesdayAM", tuesdayAMServers);
+    this.scheduleRemainder("tuesdayPM", tuesdayPMServers);
+    this.scheduleRemainder("mondayAM", mondayAMServers);
+    this.scheduleRemainder("mondayPM", mondayPMServers);
+    console.log(this.schedule);
+    for(let q=0; q<this.employees.length;q++){
+      if(this.employees[q].shiftsPerWeek > this.employees[q].shiftsScheduled){
+        this.problems.push(this.employees[q].name + " did not get the desired amount of shifts (" + this.employees[q].shiftsPerWeek + " expected, " + this.employees[q].shiftsScheduled + " received).")
       }
     }
-    console.log(schedule);
-    console.log("Potential problems with this schedule:", problems);
+    console.log(this.schedule);
+    console.log("Potential problems with this schedule:", this.problems);
   }
-  makeSchedule()
-  }
-  employees=[];
-  getEmployees(){
-  let observable= this._dataService.getEmployees();
-  observable.subscribe(data => {
-    console.log("HERE IS THE OBSERVABLE", data);
-    this.employees = data['employees'];
-    console.log("HERE ARE THE EMPLOYEES: ", this.employees);
-    return this.employees;
+  //employees=[];
+  getEmployees() {
+    let observable= this._dataService.getEmployees();
+    observable.subscribe(data => {
+        console.log("HERE IS THE OBSERVABLE", data);
+        this.employees = data['employees'];
+        console.log("HERE ARE THE EMPLOYEES: ", this.employees);
+
+        //The below code automatically assigns extra properties to each employee to help the algorithm make decisions
+        for (var i = 0; i < this.employees.length; i++) {
+            var server = this.employees[i];
+            //We keep track of the number of shifts that have been scheduled this week so that the limits will not be exceeded.
+            server.shiftsScheduled = 0;
+            server.bartenderScheduled = 0;
+            server.shiftLeaderScheduled = 0;
+            server.availability = 0;
+            for (var key in server.shifts) {
+                if (server.requests.indexOf(key.toString()) > -1) {
+                    server.shifts[key] = false;
+                }
+                if (server.shifts[key] === true) {
+                    server.availability++;
+                }
+            }
+            server.priority = server.shiftsPerWeek + 14 - server.availability;
+            //Priority decides who is scheduled when multiple employees can work the same shift. An employee will receive higher priority if there are less other shifts available for them, to guarantee that someone who can only work one particular shift will always get it. An employee who can work more shifts per week will receive higher priority; their priority will be higher than that of an employee with open availability who can only work once a week, but lower than that of a server who can only work one specific shift.
+        }
+        this.makeSchedule();
     })
-  }
+}
 }
