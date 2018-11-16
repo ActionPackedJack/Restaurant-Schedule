db797f2d4befa61d711afae3679982f128171ea0
ActionPackedJack <>
2018-11-16 16:59:59 -0600

    Added some files necessary for Heroku deployment.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
+app.set('port', (process.env.PORT || 5000));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}))
 var path = require('path');
@@ -9,4 +10,6 @@ app.set('views', path.join(__dirname, '/public/dist'))
 require('./server/config/mongoose.js');
 var routes_setter = require('./server/config/routes.js');
 routes_setter(app);
-app.listen(process.env.PORT || 5000)
+app.listen(app.get('port'), function(){
+    console.log('Node app is running on port', app.get('port'))
+});
